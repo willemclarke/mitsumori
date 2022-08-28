07ef8e5753ffbd985d6d1ee1ca913c8644fc2431
Willem Clarke <willem.2@hotmail.com>
2022-08-28 11:05:20 +1000

    move Main.elm.d.ts, index.d.ts, main.ts into src

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Elm } from "./src/Main.elm";
+import { Elm } from "./Main.elm";
 
 if (process.env.NODE_ENV === "development") {
   const ElmDebugTransform = await import("elm-debug-transformer");
